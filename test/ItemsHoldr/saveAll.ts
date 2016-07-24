bafba74da1c8220c553f6fbf6f8c974c15632e78
Josh Goldberg <josh@fullscreenmario.com>
2016-07-24 15:37:24 -0700

    Bumped gulp-shenanigans to 0.3.1

--- test/ItemsHoldr/saveAll.ts
+++ test/ItemsHoldr/saveAll.ts
@@ -1,8 +1,8 @@
 /// <reference path="../../node_modules/@types/chai/index.d.ts" />
 /// <reference path="../../node_modules/@types/mocha/index.d.ts" />
 /// <reference path="../../lib/ItemsHoldr.d.ts" />
-/// <reference path="../utilities/MochaLoader.ts" />
-/// <reference path="../utilities/mocks.ts" />
+/// <reference path="../utils/MochaLoader.ts" />
+/// <reference path="../utils/mocks.ts" />
 
 mochaLoader.addTest("saves changes to items to localStorage", (): void => {
     // Arrange
